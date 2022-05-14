8dec30f73ba81c69fb2f894808c109c60a111da9
刘斯坦 <liusitan@hiroshis-MacBook-Pro.local>
2022-05-14 09:19:06 -0700

    submit

--- src/main.c
+++ src/main.c
@@ -83,7 +83,7 @@ main(int argc, char *argv[])
   stream = stdin;
   bpType = STATIC;
   verbose = 0;
-  
+
   // Process cmdline Arguments
   for (int i = 1; i < argc; ++i) {
     if (!strcmp(argv[i],"--help")) {
